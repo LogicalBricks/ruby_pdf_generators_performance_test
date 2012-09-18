37233b2f3e9d5e3001cdc2d7992cf07bfad70cf7
Thot <thot@logicalbricks.(none)>
2012-09-18 14:50:33 -0500

    Testing wkhtml to pdf with ERB processing

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 ruby_pdf_generators_performance_test
 ====================================
 
-Performance test with PDF Generators available in Ruby
+Group of performance test using ERB and PDF generators.
+
+The main goal is to get information to choice the best PDF generator for our project.
+
+Tested PDF generators:
+- wkhtmltopdf (executing the command)
+- rtex (Using a latex layout)
+- acts_as_flying_saucer (Without nailgun)
+- acts_as_flying_saucer (With nailgun)
+
+Testing with wkhtmltopdf
+------------------------
+
+In ubuntu you need to install
+
+    sudo apt-get install wkhtmltopdf
+
--- test_acts_as_flying_saucer.rb
+++ test_acts_as_flying_saucer.rb
@@ -2,22 +2,21 @@ require 'rubygems'
 require 'acts_as_flying_saucer'
 require 'digest/sha1'
 
-input_file = './hola.html'
-output_file = './hola.pdf'
+input_file = 'index.html'
+output_file = 'results/test-aafs2.pdf'
 
 class Pdf
-  #include ActsAsFlyingSaucer::Controller
-  #acts_as_flying_saucer
+  include ActsAsFlyingSaucer::Controller
+  acts_as_flying_saucer
   def generate_pdf(input_file_html,output_pdf)
     options = ActsAsFlyingSaucer::Config.options.merge({:url=>input_file_html,:pdf_file=>output_pdf})
-    #render_pdf(options)
-    ActsAsFlyingSaucer::Xhtml2Pdf.write_pdf(options)
+    render_pdf(options)
+    #ActsAsFlyingSaucer::Xhtml2Pdf.write_pdf(options)
   end
 end
 
 generator = Pdf.new
 file = File.read(input_file)
-
 generator.generate_pdf(input_file, output_file)
 
 
